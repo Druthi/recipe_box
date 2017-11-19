3edb9fab49b0160396c7818abe5d5196ff79f96b
Druthi Polisetty <polisettydruthi@gmail.com>
2017-11-19 20:04:44 +0530

    Inputs recipe name and ingridients and stores in an object

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,27 +1,72 @@
 import React, { Component } from 'react';
 import { Input, Button } from 'semantic-ui-react';
+import uuidv4 from 'uuid/v4';
 
 class AddRecipe extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      recipes: {
-        name:"",
-        ingridients:""
+      recipes: {},
+      inputValue: {
+          name:"",
+          ingridients:""
       }
     }
   }
+
+  onRecipeChange= (e) => {
+      //console.log(e.target.value);
+      this.setState({...this.state, 
+        inputValue:{ ...this.state.inputValue,
+            name:e.target.value
+        }
+    });
+  }
+
+  onIngridientsChange= (e) => {
+    //console.log(e.target.value);
+    this.setState({...this.state, 
+      inputValue:{ ...this.state.inputValue,
+          ingridients:e.target.value
+      }
+    });
+  }
+
+  createRecipe= (e) => {
+      //console.log(e)
+    e.preventDefault();
+    const { inputValue, recipes } = this.state;
+    const recipeId = uuidv4();
+      this.setState({...this.state,
+        recipes: {
+            ...recipes,
+            [recipeId]: { 
+                recipeId: recipeId,
+                name:inputValue.name,
+                ingridients:inputValue.ingridients
+            }            
+        },
+        inputValue:{
+            name:"",
+            ingridients:""
+        }
+    });
+    console.log(recipes);
+  }
+
   render() {
+      const { inputValue, recipes } = this.state;
     return (
       <div className="AddRecipe">
-        <form>
+        <form onSubmit={this.createRecipe}>
           <p>Recipe</p>
-          <Input placeholder='Recipe Name' value={this.state.name}/>
+           <Input placeholder='Recipe Name' onChange={this.onRecipeChange} value={inputValue.name}/>
           <p>Ingridients</p>
-          <Input placeholder='Enter ingridients separated by commas' value={this.state.ingridients} size="large"/>
+          <Input placeholder='Enter ingridients separated by commas' onChange={this.onIngridientsChange} value={inputValue.ingridients} size="large"/>
+       
+            <Button primary>Add Recipe</Button>
         </form>
-        <Button primary>Add Recipe</Button>
         <Button basic>Close</Button>
         
         
