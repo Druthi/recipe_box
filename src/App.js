0718e6b7a670a6a3046c7c22ecea0cfa3cccd339
Druthi Polisetty <polisettydruthi@gmail.com>
2017-11-25 01:04:38 +0530

    Edit button works

--- src/App.js
+++ src/App.js
@@ -1,132 +1,156 @@
 import React, { Component } from 'react';
-//import AddRecipe from './components/AddRecipe';
 import IngridientsList from './components/IngridientsList';
 import { Well, Form, FormGroup, FormControl, Button, Accordion, Modal } from 'react-bootstrap';
-//import { Input } from 'semantic-ui-react';
 import uuidv4 from 'uuid/v4';
 import { pickBy } from 'lodash';
 
-//const Ingridient= ({id, name, ingridients}) => {
-//  const ingridientList= ingridients.split(",");
-//  return(
-//    <Accordion.Title active={activeIndex === 0} index={0} onClick={this.handleClick}>
-//          <Icon name='dropdown' />
-//          What is a dog?
-//    </Accordion.Title>
-//    <Accordion.Content active={activeIndex === 0}>
-//          <p>
-//            A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a
-//            {' '}welcome guest in many households across the world.
-//          </p>
-//    </Accordion.Content>
-//  );
-//};
-
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       showModal: false,
+      editModal: false,
       recipes: {},
       inputValue: {
-          name:"",
-          ingridients:""
-      }
+        name: "",
+        ingridients: ""
+      },
+      editId: 0
     }
 
   }
 
-  onRecipeChange= (e) => {
-      //console.log(e.target.value);
-      this.setState({...this.state, 
-        inputValue:{ ...this.state.inputValue,
-            name:e.target.value
-        }
-    });
-  }
-
-  onIngridientsChange= (e) => {
-    //console.log(e.target.value);
-    this.setState({...this.state, 
-      inputValue:{ ...this.state.inputValue,
-          ingridients:e.target.value
+  onRecipeChange = (e) => {
+    this.setState({
+      ...this.state,
+      inputValue: {
+        ...this.state.inputValue,
+        name: e.target.value
       }
     });
   }
 
-      
-  createRecipe= (e) => {
-      console.log(e)
+  onIngridientsChange = (e) => {
+    //console.log(e.target.value);
+    this.setState({
+      ...this.state,
+      inputValue: {
+        ...this.state.inputValue,
+        ingridients: e.target.value
+      }
+    });
+  }
+
+
+  createRecipe = (e) => {
+    console.log(e)
     e.preventDefault();
     const { inputValue, recipes } = this.state;
     const recipeId = uuidv4();
-      this.setState({...this.state,
-        recipes: {
-            ...recipes,
-            [recipeId]: { 
-                recipeId: recipeId,
-                name:inputValue.name,
-                ingridients:inputValue.ingridients
-            }            
-        },
-        inputValue:{
-            name:"",
-            ingridients:""
+    this.setState({
+      ...this.state,
+      recipes: {
+        ...recipes,
+        [recipeId]: {
+          recipeId: recipeId,
+          name: inputValue.name,
+          ingridients: inputValue.ingridients
         }
-        
+      },
+      inputValue: {
+        name: "",
+        ingridients: ""
+      }
+
     });
     console.log(Object.entries(recipes));
     //console.log(recipes);
   }
 
-  deleteRecipe = (id) =>{
+  deleteRecipe = (id) => {
     const { recipes } = this.state;
     const newObj = pickBy(
       recipes,
       ({ recipeId }) => recipeId !== id
     );
-    this.setState({ recipes: newObj});
+    this.setState({ recipes: newObj });
     //console.log(Object.entries(newObj));
 
   }
 
-  editRecipe= (id) =>{
+  editRecipe = (id) => {
+    const { recipes, inputValue } = this.state;
+    const recipe = pickBy(
+      recipes,
+      ({ recipeId }) => recipeId === id
+    );
+
+    this.setState({
+      editModal: true, editId: id,
+      inputValue: {
+        name: recipe[id].name,
+        ingridients: recipe[id].ingridients
+      }
+    });
+    console.log(this.state.editModal, inputValue);
+  }
+
+  finalEdit = (id) => {
+    const { inputValue, recipes } = this.state;
+    this.setState({
+      ...this.state,
+      recipes: {
+        ...recipes,
+        [id]: {
+          recipeId: id,
+          name: inputValue.name,
+          ingridients: inputValue.ingridients
+        }
+      },
+      editModal: false
+    });
+    console.log(Object.entries(recipes));
+
 
   }
 
   close = () => {
-    this.setState({ showModal: false });
+    this.setState({ showModal: false, editModal: false });
   }
 
   open = () => {
-    this.setState({ showModal: true });
+    this.setState({ showModal: true, editModal: true });
   }
 
- //
-//
-  //onClick= () => {
-  //  this.dialog.showAlert('Hello Dialog!')
-  //}
-//
-  //componentWillMount = () =>{
-  //  Modal.setAppElement('body');
-  //}
-//
-//
-  //toggleModal = () => {
-  //  this.setState({...this.state,
-  //    isActive: !this.state.isActive
-  //  });
-  //}
-//
- 
+
+
   render() {
     const { inputValue, recipes } = this.state;
     return (
-      
+
       <div className="App">
         <section>
-          <Button bsStyle="primary" onClick={this.open}> Show Modal </Button>
+          <Modal show={this.state.editModal} onHide={this.close}>
+            <Modal.Header closeButton>
+              <Modal.Title>Edit Recipe</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form>
+                <p>Recipe</p>
+                <FormGroup >
+                  <FormControl type="text" placeholder='Recipe Name' onChange={this.onRecipeChange} value={inputValue.name} />
+                </FormGroup>
+                <p>Ingridients</p>
+                <FormGroup >
+                  <FormControl type="text" placeholder='Enter ingridients separated by commas' onChange={this.onIngridientsChange} value={inputValue.ingridients} size="large" />
+                </FormGroup>
+              </Form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={() => this.finalEdit(this.state.editId)} bsStyle="primary">Edit Recipe</Button>
+              <Button onClick={this.close}>Close</Button>
+            </Modal.Footer>
+          </Modal>
           <Modal show={this.state.showModal} onHide={this.close}>
             <Modal.Header closeButton>
               <Modal.Title>Add Recipe</Modal.Title>
@@ -134,20 +158,21 @@ class App extends Component {
             <Modal.Body>
               <Form>
                 <p>Recipe</p>
-                  <FormGroup>
-                    <FormControl type="text" placeholder='Recipe Name' onChange={this.onRecipeChange} value={inputValue.name}/>
-                  </FormGroup>
+                <FormGroup>
+                  <FormControl type="text" placeholder='Recipe Name' onChange={this.onRecipeChange} value={inputValue.name} />
+                </FormGroup>
                 <p>Ingridients</p>
-                  <FormGroup>
-                    <FormControl type="text" placeholder='Enter ingridients separated by commas' onChange={this.onIngridientsChange} value={inputValue.ingridients} size="large"/>       
-                  </FormGroup>                  
+                <FormGroup>
+                  <FormControl type="text" placeholder='Enter ingridients separated by commas' onChange={this.onIngridientsChange} value={inputValue.ingridients} size="large" />
+                </FormGroup>
               </Form>
             </Modal.Body>
             <Modal.Footer>
               <Button onClick={this.createRecipe} bsStyle="primary">Add Recipe</Button>
               <Button onClick={this.close}>Close</Button>
             </Modal.Footer>
-          </Modal>          
+          </Modal>
+          <Button bsStyle="primary" onClick={this.open}> Add Recipe </Button>
         </section>
         <div>
           <Well>
@@ -155,11 +180,11 @@ class App extends Component {
               <IngridientsList
                 key={id}
                 name={name}
-                ingridients={ingridients}  
-                index={index}   
+                ingridients={ingridients}
+                index={index}
                 delete={this.deleteRecipe}
-                edit={this.editRecipe} 
-                id={id}      
+                edit={this.editRecipe}
+                id={id}
               />
             ))}
           </Well>
